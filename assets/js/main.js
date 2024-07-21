f16cbd6573f76213224ed840f7377edf26008802
NeiJunio <neijunio50@gmail.com>
2024-07-21 00:40:45 -0300

    commit

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+
+function updateProfileInfo(profileData){
+    document.getElementsByClassName('.photo')
+}
+
+
+(async (done) => {
+    const profileData = fetchProfileData()
+    console.log(profileData)
+})()
